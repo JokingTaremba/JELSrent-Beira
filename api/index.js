b8892cf8277389dab7ee09e56e0dd5546a0940a9
JokingTaremba <jokingtaremba@gmail.com>
2024-05-03 04:35:38 +0200

    conectando banco de dados

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("conectado a base de dados");
+    }).catch((err) =>{
+        console.log(err);
+    });
 
 const app = express();
 app.listen(3000, () => {
