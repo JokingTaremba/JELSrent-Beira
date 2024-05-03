34e18aa7c3ca89987f35e38f2dbda175a8a1c4ba
JokingTaremba <jokingtaremba@gmail.com>
2024-05-03 17:11:52 +0200

    criando middleware e tratando possiveis erros da funcao

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import { error } from 'console';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -23,3 +24,14 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter); 
+
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        succes: false,
+        statusCode,
+        message,
+    });
+});
