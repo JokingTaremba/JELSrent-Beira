71b36f7bc5b79fedcbfbfd17cf66f207cc7209cd
JokingTaremba <jokingtaremba@gmail.com>
2024-05-08 02:33:45 +0200

    adicionando funcionalidade de delete user

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -32,3 +32,14 @@ export const updateUser = async(req, res, next) => {
         next(error)
     }
 };
+
+export const deleteUser = async(req, res, next) => {
+    if(req.user.id !== req.params.id) return next(errorHandler(401, 'Você somente pode deletar a sua conta!'))
+    try {
+        await User.findByIdAndDelete(req.params.id)
+        res.clearCookie('access_token');
+        res.status(200).json('Usuario foi deletado!');
+    } catch (error) {
+        next(error)
+    }
+}
