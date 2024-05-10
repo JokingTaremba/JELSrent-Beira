de097db2580b8ead2be139c8084a0656f9390ce1
JokingTaremba <jokingtaremba@gmail.com>
2024-05-10 22:38:16 +0200

    criando get user listings api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import bcryptjs from 'bcryptjs';
 import User from '../models/user.model.js';
 import { errorHandler } from '../utils/error.js';
+import Listing from '../models/listing.model.js';
 export const test = (req, res) => {
     res.json({
         message: 'A api route esta a correr!',
@@ -41,5 +42,19 @@ export const deleteUser = async(req, res, next) => {
         res.status(200).json('Usuario foi deletado!');
     } catch (error) {
         next(error)
+    }
+};
+
+export const getUserListings = async (req, res, next) => {
+    if(req.user.id === req.params.id){
+        try {
+            const listings = await Listing.find({userRef: req.params.id});
+            res.status(200).json(listings);
+        } catch (error) {
+            next(error);
+        }
+
+    }else{
+        return next(errorHandler(401, 'Você só pode ver a sua listagem'));
     }
 }
