8b1dbbf0ad45263f0356aee634902c6edc2e2c07
JokingTaremba <jokingtaremba@gmail.com>
2024-05-14 01:08:25 +0200

    completando about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>Sobre JELSrent</h1>
+      <p className='mb-4 text-slate-700'>Bem-vindo a JELSrent, sua plataforma completa
+       para venda e aluguel de imóveis. Nosso objetivo é facilitar a vida de compradores, vendedores, 
+       proporcionando uma experiência segura, eficiente e agradável em todas as etapas do processo imobiliário.</p>
+      <p className='mb-4 text-slate-700'>
+      Nossa missão é transformar o mercado imobiliário, oferecendo um serviço inovador que conecta 
+      pessoas a seus lares ideais de forma rápida e descomplicada. Valorizamos a transparência,
+       a confiança e a satisfação dos nossos clientes, e estamos comprometidos em fornecer as melhores 
+       ferramentas e suporte para que você encontre o imóvel perfeito.
+      </p>
+    </div>
   )
 }
